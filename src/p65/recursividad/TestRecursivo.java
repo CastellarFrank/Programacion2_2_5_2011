7d6219f0d635b98844d6afd111fa4c554ecec74d
Carlos Gochez <carlos.gochez@unitec.edu>
2011-10-14 07:11:16 -0600

    primer commit

---
+++ src/p65/recursividad/TestRecursivo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p65.recursividad;
+
+/**
+ *
+ * @author Gotcha
+ */
+public class TestRecursivo {
+    public static void main(String args[]){
+        System.out.println("hola");
+    }
+}
