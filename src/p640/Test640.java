c237e28ab6d9cb889553b05c29186d582b628d81
Carlos Gochez <carlos.gochez@unitec.edu>
2011-10-14 19:27:41 -0600

    add a class

---
+++ src/p640/Test640.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p640;
+
+/**
+ *
+ * @author Gotcha
+ */
+public class Test640 {
+    public static void main(String args[]){
+        System.out.println("Hola 640");
+    }
+}
